d8ba951a9c3511dc51ee9ab3af60b9346f0b1ac9
Akshay  Chavan <akshay.chavan@fujitsu.com>
2021-03-19 15:34:54 +0530

    Added test cases

---
+++ SwiftyLibAkshay/SwiftyLibAkshay.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLibAkshay.swift
+//  SwiftyLibAkshay
+//
+//  Created by Akshay  Chavan on 19/03/21.
+//
+
+import Foundation
+
+public final class SwiftyLibAkshay {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    //
+}
--- SwiftyLibAkshayTests/SwiftyLibAkshayTests.swift
+++ SwiftyLibAkshayTests/SwiftyLibAkshayTests.swift
@@ -10,9 +10,21 @@ import XCTest
 
 class SwiftyLibAkshayTests: XCTestCase {
 
+    var swiftyLib: SwiftyLibAkshay!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = SwiftyLibAkshay()
     }
+    
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub()  {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 4), -2)
+    }
+
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
@@ -21,13 +33,6 @@ class SwiftyLibAkshayTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
