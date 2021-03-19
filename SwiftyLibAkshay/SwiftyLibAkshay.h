0c8bc781dd6dc3e1bf95e505a4f65b14694a51cf
Akshay  Chavan <akshay.chavan@fujitsu.com>
2021-03-19 14:37:04 +0530

    Initial Commit

---
+++ SwiftyLibAkshay/SwiftyLibAkshay.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLibAkshay.h
+//  SwiftyLibAkshay
+//
+//  Created by Akshay  Chavan on 19/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibAkshay.
+FOUNDATION_EXPORT double SwiftyLibAkshayVersionNumber;
+
+//! Project version string for SwiftyLibAkshay.
+FOUNDATION_EXPORT const unsigned char SwiftyLibAkshayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibAkshay/PublicHeader.h>
+
+
